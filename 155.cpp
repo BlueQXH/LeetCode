61d07dbde18dc50700518841600d3c44aa58159f
BlueQXH <kobe.792244049@qq.com>
2018-11-24 10:48:21 +0800

    modified:   155.cpp

--- 155.cpp
+++ 155.cpp
@@ -1,30 +1,125 @@
-class MinStack {
+class MinStack1
+{
 public:
-    void push(int x) {
+    void push(int x)
+	{
         data_st.push(x);
         if (data_st.empty() || x < min_st.top())
+		{
             min_st.push(x);
+		}
         else
+		{
             min_st.push(min_st.top());
+		}
     }
     
-    void pop() {
+    void pop()
+	{
         if (data_st.empty())
+		{
             return ;
+		}
         data_st.pop();
         min_st.pop();
     }
     
-    int top() {
+    int top()
+	{
         return data_st.top();
     }
     
-    int getMin() {
+    int getMin()
+	{
         return min_st.top();
     }
 private:
     stack<int> min_st; //辅助栈
     stack<int> data_st;
+};
+
+class MinStack2
+{
+public:
+    /** initialize your data structure here. */
+    MinStack() {}
+    
+    void push(int x)
+    {
+        if (!st.empty())
+        {
+            int val = st.top().second;
+            if (val < x)
+            {
+                st.push(make_pair(x, val));
+                return ;
+            }
+        }
+        st.push(make_pair(x, x));
+    }
+    
+    void pop()
+    {
+        st.pop();
+    }
+    
+    int top()
+    {
+        return st.top().first;
+    }
+    
+    int getMin()
+    {
+        return st.top().second;
+    }
+private:
+    stack<pair<int, int>> st;
+};
+
+class MinStack3
+{
+public:
+    /** initialize your data structure here. */
+    MinStack(){}
+    
+    void push(int x)
+    {
+        if (st.empty())
+        {
+            st.push(0);
+            Min = x;
+        }
+        else
+        {
+            st.push(x-Min);
+            if (x < Min)
+            {
+                Min = x;
+            }
+        }
+    }
+    
+    void pop()
+    {
+        if (st.top() < 0)
+        {
+            Min -= st.top();
+        }
+        st.pop();
+    }
+    
+    int top()
+    {
+        return st.top() < 0 ? Min : st.top()+Min;
+    }
+    
+    int getMin()
+    {
+        return Min;
+    }
+private:
+    stack<int> st;
+    int Min;
 };
 
 /**
@@ -35,46 +130,3 @@ private:
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
-
-struct element
-{
-    element(int v) : val(v) {};
-    int val = 0;
-    int min = 0;
-};
-
-class MinStack
-{
-public:
-    /** initialize your data structure here. */
-    MinStack() {
-        
-    }
-    
-    void push(int x) {
-        element e(x);
-        if (st.empty())
-        {
-            e.min = x;
-        }
-        else
-        {
-            e.min = x < st.top().min ? x : st.top().min;
-        }
-        st.push(move(e));
-    }
-    
-    void pop() {
-        st.pop();
-    }
-    
-    int top() {
-        return st.top().val;
-    }
-    
-    int getMin() {
-        return st.top().min;
-    }
-private:
-    stack<element> st;
-};
